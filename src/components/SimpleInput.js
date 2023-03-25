cef6f9db1dc6a861e83ff847c551294c5b71e9c6
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-03-25 16:53:10 +0300

    From Validation actually dirty for now ...

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,71 @@
+import React, { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [inputTouched, setInputTouched] = useState(false);
+
+
+  
+
+  const nameRef = useRef();
+
+  const enterNameHandler = (event) => {
+    setEnteredName(event.target.value);
+    if (event.target.value.trim() !== "") {
+      setIsValid(true);
+
+      console.log(isValid)
+
+  }
+};
+
+const userBlurHandler = event => {
+  setInputTouched(true)
+    if (enteredName.trim() === "") {
+      setIsValid(false);
+
+      
+    } 
+
+}
+
+
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    setInputTouched(true)
+    if (enteredName.trim() === "") {
+      setIsValid(false);
+      return;
+    }
+    setIsValid(true);
+   
+    setEnteredName("");
+  };
+
+  let inputIsInValid = (!isValid && inputTouched);
+console.log(inputIsInValid)
+
+
+  const formValidation = `${
+    (inputIsInValid)? "invalid input form-control" : "form-control"
+  }`;
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitFormHandler}>
+      <div className={formValidation}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={enterNameHandler}
+          onBlur={userBlurHandler}
+        />
+        {(inputIsInValid) && <p className="error-text">Name is not valid!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
